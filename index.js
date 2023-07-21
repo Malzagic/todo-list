e8e373971f32165221d1b648398306080e27324c
Malzagic <przemek2142@gmail.com>
2023-07-22 00:04:09 +0200

    uninstall store2 package

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import express from "express";
 import bodyParser from "body-parser";
-import store from "store2";
 
 const app = express();
 const port = 8000;
@@ -11,8 +10,6 @@ app.use(parser);
 
 app.get("/", (req, res) => {
     const arr = [];
-
-    console.log(store.get("name"));
 
     const data = {
         title: 'ToDo List - App',

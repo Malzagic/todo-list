77d395981955b54a87a4f86eb9fdab09ba94a284
Malzagic <przemek2142@gmail.com>
2023-07-21 22:46:12 +0200

    inisital commit

---
+++ index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 8000;
+const parser = bodyParser.urlencoded({extended: true});
+
+app.use(express.static("public"));
+app.use(parser);
+
+app.get("/", (req, res) => {
+    const data = {
+        title: 'ToDo List - App'
+    }
+
+    res.render("index.ejs", {data: data});
+});
+
+app.listen(port, () => {
+    console.log(`Server is running at: ${port} port.`)
+});

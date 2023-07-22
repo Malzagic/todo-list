7af539227a3b016298b28bf895181f093b087e18
Malzagic <przemek2142@gmail.com>
2023-07-22 08:26:07 +0200

    modify for using mongodb & get first data from data base

--- index.js
+++ index.js
@@ -15,34 +15,45 @@ const password = process.env.MONGODB_PASSWORD;
 
 const url = `mongodb+srv://${login}:${password}@todolist.reaw3ux.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(url);
+const dbName = "list";
 
 
-async function run(req, res, next) {
-    try {
-        await client.connect();
-        console.log("Successfully connected to Atlas");
-    } catch (err) {
-        console.log(err.stack);
-    }
-    finally {
-        await client.close();
-    }
+// async function run(req, res, next) {
+//     try {
+//         await client.connect();
+//         console.log("Successfully connected to Atlas");
+//     } catch (err) {
+//         console.log(err.stack);
+//     }
+//     finally {
+//         await client.close();
+//     }
 
-    next();
-}
+//     next();
+// }
 
 
 
 app.use(express.static("public"));
 app.use(parser);
 
-app.use(run);
+// app.use(run);
 
-app.get("/", (req, res) => {
-
+app.get("/", async (req, res) => {
     const data = {
         title: 'ToDo List - App',
         list: [],
+    }
+
+    try {
+        await client.connect();
+        const db = client.db(dbName);
+        const col = db.collection("todo");
+
+        const myDoc = await col.findOne();
+        data.list.push(myDoc);
+    } catch (err) {
+        console.log(err.stack);
     }
 
     res.render("index.ejs", { data: data });

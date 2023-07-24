e54ea7469d5b2cdec697814eae5a2c72a3f8c8ff
Malzagic <przemek2142@gmail.com>
2023-07-24 18:19:48 +0200

    repair git commits

--- index.js
+++ index.js
@@ -16,17 +16,9 @@ const password = process.env.MONGODB_PASSWORD;
 const url = `mongodb+srv://${login}:${password}@todolist.reaw3ux.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(url);
 const dbName = "list";
-<<<<<<< HEAD
 
 const title = 'ToDo List - App';
 let data = [];
-=======
-const data = {
-    _id: '',
-    title: 'ToDo List - App',
-    list: [],
-}
->>>>>>> 74043cc483d5927568df07025eedbba14787accc
 
 
 async function updateList(req, res, next) {
@@ -51,18 +43,11 @@ app.use(express.static("public"));
 app.use(parser);
 app.use(updateList);
 
-<<<<<<< HEAD
 app.get("/", (req, res, next) => {
     res.render("index.ejs", {data: data, title: title});
     next();
 });
 
-=======
-app.get("/", (req, res) => {
-
-    res.render("index.ejs", { data: data });
-});
->>>>>>> 74043cc483d5927568df07025eedbba14787accc
 
 app.post("/submit", async (req, res, next) => {
     const { name, todo } = req.body;
@@ -90,16 +75,12 @@ app.post("/submit", async (req, res, next) => {
 });
 
 app.delete("/delete", async (req, res, next) => {
-<<<<<<< HEAD
     const { itemID, itemDataID } = req.body;
 
-=======
->>>>>>> 74043cc483d5927568df07025eedbba14787accc
     try {
         await client.connect();
         const db = client.db(dbName);
         const col = db.collection("todo");
-<<<<<<< HEAD
         const objectID = new ObjectId(itemID);
         const filter = { _id: objectID };
 
@@ -115,14 +96,6 @@ app.delete("/delete", async (req, res, next) => {
         console.log(err.stack);
     }
 
-=======
-
-        console.log(req.body.id)
-        // const filter = { _id: new ObjectID(itemId) };
-    } catch(err) {
-        console.log(err.stack);
-    }
->>>>>>> 74043cc483d5927568df07025eedbba14787accc
 });
 
 app.listen(port, () => {
